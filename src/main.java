b245f4793f172a256913769ca9e91e2147c32798
katrinesando <kmsa@itu.dk>
2022-11-17 10:21:08 +0100

    made new function

--- src/main.java
+++ src/main.java
@@ -2,4 +2,10 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hi");
     }
+    public int cal (int num){
+        if(num%2==0){
+            return 1;
+        }
+        return 0;
+    }
 }
